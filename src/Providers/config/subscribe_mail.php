abf8100f8879bd86abd716e60b74af2f636f3436
IEugeneI <eugenelboichenko@gmail.com>
2023-06-28 11:21:40 +0300

    fix

---
+++ src/Providers/config/subscribe_mail.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+    //Mailchimp,Brevo,CampaignMonitor
+    'provider'=>'Brevo',
+
+
+
+
+
+
+
+    'mailchimp'=>[
+        'apiKey'=>'123',
+        'server'=>'123',
+    ],
+
+
+    'brevo'=>[
+        'apiKey'=>'123'
+    ],
+
+
+    'campaignmonitor'=>[
+        'apiKey'=>'123',
+        'client_id'=>'123'
+    ]
+];
