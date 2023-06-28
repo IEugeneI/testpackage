8cc46bfa39c1b2b12e96cc319907627f42819b5c
IEugeneI <eugenelboichenko@gmail.com>
2023-06-28 10:43:20 +0300

    service prodvider

---
+++ src/Providers/NewsletterProvider.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Fhsinchy\Inspire\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class NewsletterProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot():void
+    {
+        $this->publishes([
+            __DIR__.'/../config/subscribe_mail.php' => config_path('subscribe_mail.php'),
+        ]);
+    }
+}
